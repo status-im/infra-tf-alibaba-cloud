ecd6329cedfd6f2c411b64b74923514531dbb686
Jakub Sokołowski <jakub@status.im>
2018-11-06 18:15:42 +0100

    add EIP for hosts

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "alicloud_security_group" "host" {
   vpc_id      = "${data.alicloud_vpcs.host.vpcs.0.id}"
 }
 
+/* WARNING: Using 'all' protocol fucks with port_range option */
 resource "alicloud_security_group_rule" "icmp" {
   security_group_id = "${alicloud_security_group.host.id}"
   type              = "ingress"
@@ -33,10 +34,19 @@ resource "alicloud_security_group_rule" "icmp" {
   cidr_ip           = "0.0.0.0/0"
 }
 
-resource "alicloud_security_group_rule" "host_tcp" {
+resource "alicloud_security_group_rule" "tcp" {
   security_group_id = "${alicloud_security_group.host.id}"
   type              = "ingress"
   ip_protocol       = "tcp"
+  cidr_ip           = "0.0.0.0/0"
+  port_range        = "${replace(element(local.open_ports, count.index), "-", "/")}"
+  count             = "${length(local.open_ports)}"
+}
+
+resource "alicloud_security_group_rule" "udp" {
+  security_group_id = "${alicloud_security_group.host.id}"
+  type              = "ingress"
+  ip_protocol       = "udp"
   cidr_ip           = "0.0.0.0/0"
   port_range        = "${replace(element(local.open_ports, count.index), "-", "/")}"
   count             = "${length(local.open_ports)}"
@@ -57,8 +67,8 @@ resource "alicloud_instance" "host" {
 
   tags = {
     stage = "${local.stage}"
-  	group = "${var.group}"
-  	env   = "${var.env}"
+    group = "${var.group}"
+    env   = "${var.env}"
   }
 
   key_name                   = "${var.key_pair}"
@@ -72,21 +82,42 @@ resource "alicloud_instance" "host" {
   instance_charge_type       = "${var.charge}"
   period_unit                = "${var.period}"
 
-  /* necessary because alicloud does not provide it */
-  /* https://github.com/radekg/terraform-provisioner-ansible/issues/54 */
-  connection {
-    host = "${self.public_ip}"
-  }
+  /* NOTE: We provision inside Elastic IP association */
+}
 
-  /* bootstraping access for later Ansible use */
+resource "alicloud_eip" "host" {
+  count      = "${var.count}"
+  lifecycle  = { prevent_destroy = true }
+}
+
+/**
+ * WARNING: This is broken when instance has a public_ip
+ * https://www.alibabacloud.com/help/doc-detail/72125.htm
+ * "The ECS instance does not have a public IP, nor is it bound to any EIP."
+ * https://www.terraform.io/docs/providers/alicloud/r/eip_association.html
+ **/
+resource "alicloud_eip_association" "host" {
+  allocation_id = "${element(alicloud_eip.host.*.id, count.index)}"
+  instance_id   = "${element(alicloud_instance.host.*.id, count.index)}"
+  count         = "${var.count}"
+
+  /**
+   * It is necessary to provision here instead of in alicloud_instance
+   * because Alibaba Cloud instances do not have public IPs by default
+   **/
   provisioner "ansible" {
+    connection {
+      host = "${element(alicloud_eip.host.*.ip_address, count.index)}"
+      user = "${var.ssh_user}"
+    }
+  
     plays {
       playbook = {
         file_path = "${path.cwd}/ansible/bootstrap.yml"
       }
       groups = ["${var.group}"]
       extra_vars = {
-        hostname         = "${var.name}-${format("%02d", count.index+1)}.${local.dc}.${var.env}.${local.stage}"
+        hostname         = "${element(alicloud_instance.host.*.host_name, count.index)}"
         ansible_ssh_user = "${var.ssh_user}"
         data_center      = "${local.dc}"
         stage            = "${local.stage}"
@@ -100,7 +131,7 @@ resource "cloudflare_record" "host" {
   domain = "${var.domain}"
   count  = "${var.count}"
   name   = "${element(alicloud_instance.host.*.host_name, count.index)}"
-  value  = "${element(alicloud_instance.host.*.public_ip, count.index)}"
+  value  = "${element(alicloud_eip.host.*.ip_address, count.index)}"
   type   = "A"
   ttl    = 3600
 }
@@ -110,7 +141,7 @@ resource "ansible_host" "host" {
   groups = ["${var.group}", "${local.dc}"]
   count = "${var.count}"
   vars {
-    ansible_host = "${element(alicloud_instance.host.*.public_ip, count.index)}"
+    ansible_host = "${element(alicloud_eip.host.*.ip_address, count.index)}"
     hostname     = "${element(alicloud_instance.host.*.host_name, count.index)}"
     region       = "${element(alicloud_instance.host.*.availability_zone, count.index)}"
     dns_entry    = "${element(alicloud_instance.host.*.host_name, count.index)}.${var.domain}"
