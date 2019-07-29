d27f86c8d6ddc408404a2452d786471ddd6ddb54
Jakub Sokołowski <jakub@status.im>
2019-07-28 20:24:37 -0400

    upgrade to Terraform 0.12
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,17 @@
-locals = {
-  public_ips = "${alicloud_eip.host.*.ip_address}"
-  hostnames  = "${alicloud_instance.host.*.host_name}"
+locals {
+  public_ips = alicloud_eip.host.*.ip_address
+  hostnames  = alicloud_instance.host.*.host_name
 }
 
 output "public_ips" {
-  value = ["${local.public_ips}"]
+  value = [local.public_ips]
 }
 
 output "hostnames" {
-  value = ["${local.hostnames}"]
+  value = [local.hostnames]
 }
 
 output "hosts" {
-  value = "${zipmap(local.hostnames, local.public_ips)}"
+  value = zipmap(local.hostnames, local.public_ips)
 }
+
