e42408fb5800e592701c21b29b122e4222939529
Jakub Sokołowski <jakub@status.im>
2020-07-07 20:27:22 +0200

    use Ubuntu 20.04 image
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@
 
 variable "cf_zone_id" {
   description = "ID of CloudFlare zone for host record."
+  type        = string
   /* We default to: statusim.net */
   default     = "14660d10344c9898521c4ba49789f563"
 }
@@ -10,39 +11,43 @@ variable "cf_zone_id" {
 
 variable "host_count" {
   description = "Number of instances to start in this region."
+  type        = number
 }
 
 variable "image" {
-  /**
-   * This image is created with Packer because Alicloud does not provide one
-   * See: https://github.com/status-im/infra-utils/tree/master/alicloud/ubuntu_1804
-   */
   description = "OS image used to create instance."
-  default     = "ubuntu_18_04_64_custom_20180719"
+  type        = string
+  /* Run: aliyun ecs DescribeImages --output 'cols=ImageName' 'rows=Images.Image[]' --pager --ImageName='ubuntu*' */
+  default     = "ubuntu_20_04_x64_20G_alibase_20200522.vhd"
 }
 
 variable "type" {
   description = "Type of instance to create."
+  type        = string
   default     = "ecs.t5-lc2m1.nano"
 }
 
 variable "zone" {
   description = "Availability Zone in which the instance will be created."
+  type        = string
   default     = "cn-hongkong-c"
 }
 
 variable "disk" {
   description = "Disk I/O optimization type."
+  type        = string
   default     = "cloud_ssd"
 }
 
 variable "max_band_out" {
   description = "Maximum outgoing bandwidth to the public network, measured in Mbps."
+  type        = number
   default     = 50
 }
 
 variable "vol_size" {
   description = "Size in GiB of an extra Volume to attach to the instance."
+  type        = number
   default     = 0
 }
 
@@ -50,50 +55,52 @@ variable "vol_size" {
 
 variable "provider_name" {
   description = "Short name of provider being used."
-
-  /* Digital Ocean */
-  default = "ac"
+  type        = string
+  default     = "ac" /* Alibaba Cloud */
 }
 
 variable "name" {
   description = "Prefix of hostname before index."
+  type        = string
   default     = "node"
 }
 
 variable "charge" {
   description = "Way in which the instance is paid for."
-
-  /* The other value is PrePaid */
-  default = "PostPaid"
+  type        = string
+  default     = "PostPaid" /* The other value is PrePaid */
 }
 
 variable "period" {
   description = "Time period in which we pay for instances."
-
-  /* The other value is Week */
-  default = "Month"
+  type        = string
+  default     = "Month" /* The other value is Week */
 }
 
 variable "group" {
   description = "Name of Ansible group to add hosts to."
+  type        = string
 }
 
 variable "env" {
   description = "Environment for these hosts, affects DNS entries."
+  type        = string
 }
 
 variable "domain" {
   description = "DNS Domain to update"
+  type        = string
 }
 
 variable "ssh_user" {
   description = "SSH user used to log in after creation."
+  type        = string
   default     = "root"
 }
 
 variable "key_pair" {
   description = "SSH key pair used to log in to instance"
-
+  type        = string
   /* WARNING I really shouldn't use my own key here */
   default = "jakub_status.im"
 }
